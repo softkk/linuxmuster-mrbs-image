24ce5d21087ca2337fbc4069fbc74cdc5af167de
Tobias Küchel <t.kuechel@humboldt-ka.de>
2019-10-06 23:54:06 +0200

    linuxmuster-mrbs: php version: 7.3.10-apache-buster

--- rebuild.sh
+++ rebuild.sh
@@ -7,4 +7,10 @@ git commit -a -m"linuxmuster-mrbs: php version: $php_version"
 docker pull php:apache
 docker inspect php:apache | grep RepoTags -A 3
 git_log=$(git log --oneline | head -1 | cut -d " " -f 1)
-echo "now build with: docker build -t hgkvplan/linuxmuster-mrbs:$php_version-$git_log ."
+echo "Press enter to build with: docker build -t hgkvplan/linuxmuster-mrbs:$php_version-$git_log ."
+read
+docker build -t hgkvplan/linuxmuster-mrbs:$php_version-$git_log .
+docker tag hgkvplan/linuxmuster-mrbs:$php_version-$git_log hgkvplan/linuxmuster-mrbs:latest
+echo "Try if :latest works for you. Then upload using"
+echo "docker push hgkvplan/linuxmuster-mrbs:$php_version-$git_log ; docker push hgkvplan/linuxmuster-mrbs:latest"
+
