b0a54625234d42ae358e88d808347d57171cd013
Tobias Küchel <t.kuechel@humboldt-ka.de>
2020-06-10 03:15:42 +0200

    linuxmuster/mrbs: php version: 7.4.6-apache-buster, mrbs version: 1.8.0

--- rebuild.sh
+++ rebuild.sh
@@ -3,19 +3,21 @@
 git status
 php_version=$(cat php_version.txt)
 grep $php_version Dockerfile || { echo "$php_version not in Dockerfile, please fix"; exit 1 ; }
-git commit -a -m"linuxmuster/mrbs: php version: $php_version" 
+mrbs_version=$(cat mrbs_version.txt)
+grep $mrbs_version Dockerfile || { echo "$mrbs_version not in Dockerfile, please fix"; exit 1 ; }
+git commit -a -m"linuxmuster/mrbs: php version: $php_version, mrbs version: $mrbs_version" 
 
 docker pull php:apache
 docker inspect php:apache | grep RepoTags -A 3
 git_log=$(git log --oneline | head -1 | cut -d " " -f 1)
-echo "Press enter to build with: docker build -t linuxmuster/mrbs:$php_version-$git_log ."
+echo "Press enter to build with: docker build -t linuxmuster/mrbs-${mrbs_version}:$php_version-$git_log ."
 read
-docker build -t linuxmuster/mrbs:$php_version-$git_log .
-docker tag linuxmuster/mrbs:$php_version-$git_log linuxmuster/mrbs:latest
+docker build -t linuxmuster/mrbs-${mrbs_version}:$php_version-$git_log .
+docker tag linuxmuster/mrbs-${mrbs_version}:$php_version-$git_log linuxmuster/mrbs-${mrbs_version}:latest
 echo "Try if :latest works for you. Then press enter to tag and upload using"
-echo "docker tag linuxmuster/mrbs:$php_version-$git_log linuxmuster/mrbs:working"
-echo "docker push linuxmuster/mrbs:$php_version-$git_log ; docker push linuxmuster/mrbs:latest"
+echo "docker tag linuxmuster/mrbs-${mrbs_version}:$php_version-$git_log linuxmuster/mrbs-${mrbs_version}:working"
+echo "docker push linuxmuster/mrbs-${mrbs_version}:$php_version-$git_log ; docker push linuxmuster/mrbs-${mrbs_version}:latest"
 read
-docker tag linuxmuster/mrbs:$php_version-$git_log linuxmuster/mrbs:working
-docker push linuxmuster/mrbs:$php_version-$git_log ; docker push linuxmuster/mrbs:latest
+docker tag linuxmuster/mrbs-${mrbs_version}:$php_version-$git_log linuxmuster/mrbs-${mrbs_version}:working
+docker push linuxmuster/mrbs-${mrbs_version}:$php_version-$git_log ; docker push linuxmuster/mrbs-${mrbs_version}:latest
 
