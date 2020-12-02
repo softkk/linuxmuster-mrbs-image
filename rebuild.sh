f91bfbe2d8a41f674792922069e733f25aee88b5
Tobias Küchel <t.kuechel@humboldt-ka.de>
2020-12-02 03:00:15 +0100

    linuxmuster/mrbs: php version: 7.4.13-apache-buster, mrbs version: 1.7.3

--- rebuild.sh
+++ rebuild.sh
@@ -10,8 +10,8 @@ mrbs_version=$(cat mrbs_version.txt)
 grep $mrbs_version Dockerfile || { echo "$mrbs_version not in Dockerfile, please fix"; exit 1 ; }
 git commit -a -m"linuxmuster/mrbs: php version: $php_version, mrbs version: $mrbs_version" 
 
-docker pull php:apache
-docker inspect php:apache | grep RepoTags -A 3
+docker pull php:7-apache
+docker inspect php:7-apache | grep RepoTags -A 3
 git_log=$(git log --oneline | head -1 | cut -d " " -f 1)
 echo "Press enter to build with: docker build -t linuxmuster/mrbs-${mrbs_version}:$php_version-$git_log ."
 read
